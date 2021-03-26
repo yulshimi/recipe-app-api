92debf4bb289704750073d6a8e1975e01c92b1df
Phillip Jo <yulshimi4@gmail.com>
2021-03-25 23:27:51 -0700

    Ingredient Model Added

--- app/recipe/tests/test_tags_api.py
+++ app/recipe/tests/test_tags_api.py
@@ -34,11 +34,11 @@ class PrivateTagsApiTests(TestCase):
         Tag.objects.create(user=self.user, name='Dessert')
         res = self.client.get(TAGS_URL)
         tags = Tag.objects.all().order_by('-name')
-        print('tags before being serialized')
-        print(tags)
+        #print('tags before being serialized')
+        #print(tags)
         serializer = TagSerializer(tags, many=True)
-        print('tags after being serialized')
-        print(serializer)
+        #print('tags after being serialized')
+        #print(serializer)
         self.assertEqual(res.status_code, status.HTTP_200_OK)
         self.assertEqual(res.data, serializer.data)
 
@@ -54,4 +54,34 @@ class PrivateTagsApiTests(TestCase):
         tags = Tag.objects.create(user=self.user, name='Comfort')
         res = self.client.get(TAGS_URL)
         self.assertEqual(len(res.data), 1)
-        self.assertEqual(res.data[0]['name'], tag.name)
+        self.assertEqual(res.data[0]['name'], tags.name)
+
+    def test_create_tag_successful(self):
+        """Test creating a new tag"""
+        payload = {'name': 'Simple', 'name_2': 'NotSimple'}
+        self.client.post(TAGS_URL, payload)
+        payload['name'] = 'Simple2'
+        self.client.post(TAGS_URL, payload)
+        payload['name'] = 'Simple3'
+        self.client.post(TAGS_URL, payload)
+        exists = Tag.objects.filter(
+            user=self.user,
+            name=payload['name']
+        ).exists()
+        self.assertTrue(exists)
+        exists = Tag.objects.filter(
+            user=self.user,
+            name='Simple'
+        ).exists()
+        self.assertTrue(exists)
+        exists = Tag.objects.filter(
+            user=self.user,
+            name='Simple2'
+        ).exists()
+        self.assertTrue(exists)
+
+    def test_create_tag_invalid(self):
+        """Test creating a new tag with invalid payload"""
+        payload = {'name': ''}
+        res = self.client.post(TAGS_URL, payload)
+        self.assertEqual(res.status_code, status.HTTP_400_BAD_REQUEST)
--- app/recipe/views.py
+++ app/recipe/views.py
@@ -4,7 +4,7 @@ from rest_framework.permissions import IsAuthenticated
 from core.models import Tag
 from recipe import serializers
 
-class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
+class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin, mixins.CreateModelMixin):
     """Manage tags in the database"""
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
@@ -12,3 +12,11 @@ class TagViewSet(viewsets.GenericViewSet, mixins.ListModelMixin):
     serializer_class = serializers.TagSerializer
     def get_queryset(self):
         return self.queryset.filter(user=self.request.user).order_by('-name')
+
+    """Validated Serializer"""
+    def perform_create(self, serializer):
+        """Create a new ingredient"""
+        print("Request Data")
+        print(self.request.data)
+        print(self.request.user)
+        serializer.save(user=self.request.user)
